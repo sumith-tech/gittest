e65f338c19fd732908e7ae180f5602ee87b93690
sumith-tech <sumith2200.ss@gmail.com>
2023-05-26 11:46:50 +0530

    classname

--- demo.js
+++ demo.js
@@ -1,5 +1,9 @@
-console.log(document.getElementById('header-title'));
-console.log(document.body)
 var item=(document.getElementsByClassName('title'))
 item[0].style.fontWeight='bold';
 item[0].style.color='green';
+
+var itemgrp=(document.getElementsByClassName('list-group-item'))
+itemgrp[2].style.backgroundColor='green'
+for(var i=0;i<itemgrp.length;i++){
+    itemgrp[i].style.fontWeight='bold'; 
+}
