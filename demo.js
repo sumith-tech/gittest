f0c90310159c26abd5c0f81060ad7241cf85f183
sumith-tech <sumith2200.ss@gmail.com>
2023-05-26 11:19:28 +0530

    class 2

--- demo.js
+++ demo.js
@@ -1,1 +1,5 @@
-console.log('hi')
+console.log(document.getElementById('header-title'));
+console.log(document.body)
+var item=(document.getElementsByClassName('title'))
+item[0].style.fontWeight='bold';
+item[0].style.color='green';
