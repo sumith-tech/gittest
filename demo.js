3389c4bdc77ad473c16e7de0da5d574debb8467a
sumith-tech <sumith2200.ss@gmail.com>
2023-05-26 13:56:40 +0530

    class 3

--- demo.js
+++ demo.js
@@ -1,9 +1,19 @@
-var item=(document.getElementsByClassName('title'))
-item[0].style.fontWeight='bold';
-item[0].style.color='green';
+// var item=(document.getElementsByClassName('title'))
+// item[0].style.fontWeight='bold';
+// item[0].style.color='green';
 
-var itemgrp=(document.getElementsByClassName('list-group-item'))
-itemgrp[2].style.backgroundColor='green'
-for(var i=0;i<itemgrp.length;i++){
-    itemgrp[i].style.fontWeight='bold'; 
+// var itemgrp=(document.getElementsByClassName('list-group-item'))
+// itemgrp[2].style.backgroundColor='green'
+// for(var i=0;i<itemgrp.length;i++){
+//     itemgrp[i].style.fontWeight='bold'; 
+// }
+
+var list2=document.querySelectorAll('.list-group-item')
+list2[1].style.color='green'
+var odd=document.querySelectorAll('li:nth-child(odd)')
+
+for(var i=0;i<odd.length;i++)
+{
+    odd[i].style.backgroundColor='green'
 }
+
