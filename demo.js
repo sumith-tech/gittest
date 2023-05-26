e5463e894a9522e17ea234ea6d2866faae7dac47
sumith-tech <sumith2200.ss@gmail.com>
2023-05-26 14:03:01 +0530

    class 4

--- demo.js
+++ demo.js
@@ -11,6 +11,8 @@
 var list2=document.querySelectorAll('.list-group-item')
 list2[1].style.color='green'
 var odd=document.querySelectorAll('li:nth-child(odd)')
+var thirditem=document.querySelector('.list-group-item:nth-child(3)');
+thirditem.style.color='green'
 
 for(var i=0;i<odd.length;i++)
 {
